4cc651419d66aa65c2916664ac7568a46c131d2c
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-11-20 14:20:49 -0800

    Lint JS, increment version.

--- static/app.js
+++ static/app.js
@@ -1,12 +1,10 @@
-'use strict';
-
-if(!navigator.onLine){
+if (!navigator.onLine) {
   location.href = '/offline.html';
-};
+}
 
 if (!window.localStorage) {
   console.log('Your browser does not support indexedDB');
-};
+}
 
 //Declare Constants
 const $map = document.getElementById('map');
@@ -18,19 +16,19 @@ const $loading = document.getElementById('loading');
 const currentDate = new Date();
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {scope: '/'})
-  .then(registration => {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+  .then(function () {
     console.log('[SW] Registered');
   })
   .catch(err => {
     console.warn('Error', err);
-  })
-};
+  });
+}
 
-$query.addEventListener('click', function() {
+$query.addEventListener('click', function () {
   let lat;
   let long;
-  if(localStorage.getItem('lat')) {
+  if (localStorage.getItem('lat')) {
     console.log('IN storage');
     lat = localStorage.getItem('lat');
     long = localStorage.getItem('long');
@@ -39,22 +37,23 @@ $query.addEventListener('click', function() {
     query(lat, long);
   } else {
     console.log('Out of storage');
-    getLocation(lat, long);
+    getLocation();
   }
+
   $loading.style.display = 'flex';
 });
 
-function getLocation(lat, long) {
-  if ("geolocation" in navigator) {
+function getLocation() {
+  if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(position => {
       let lat = position.coords.latitude;
       let long = position.coords.longitude;
       localStorage.setItem('lat', lat);
       localStorage.setItem('long', long);
-      query(lat, long)
+      query(lat, long);
     });
   } else {
-    alert('Your browser does not support geolocation :(');
+    console.warn('Your browser does not support geolocation :(');
   }
 }
 
@@ -64,38 +63,45 @@ function query(lat, long) {
   let years = $years.value;
   let radius = $radius.value;
 
-  const startTime = (currentDate.getFullYear() - years) + '-' + currentDate.getMonth() + '-' + currentDate.getDay();
-  const endTime = currentDate.getFullYear() + '-' + (currentDate.getMonth()) + '-' + currentDate.getDay();
-  let url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&";
-  let finalURL = `${url}maxradius=${radius}&minmagnitude=${mag}&latitude=${lat}&longitude=${long}&starttime=${startTime}&endtime=${endTime}`;
+  const startTime = (currentDate.getFullYear() - years) + '-' +
+   currentDate.getMonth() + '-' + currentDate.getDay();
 
-  fetch(finalURL, { method: 'GET'})
+  const endTime = currentDate.getFullYear() + '-' +
+   (currentDate.getMonth()) + '-' + currentDate.getDay();
+
+  let url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&';
+  let finalURL =
+  `${url}maxradius=${radius}&minmagnitude=${mag}
+  &latitude=${lat}&longitude=${long}&starttime=${startTime}&endtime=${endTime}`;
+
+  fetch(finalURL, { method: 'GET' })
     .then(resp => {
       resp.json().then(data => {
         let earthquakeData = data;
         createMap(earthquakeData, lat, long, radius);
-      })
+      });
     })
     .catch(err => {
       console.warn('Fetch error: ', err);
-    })
-};
+    });
+}
 
 //Create the map with the google maps api
 function createMap(data, lat, long, radius) {
-  let myLatLng = {lat: lat, lng: long};
+  let myLatLng = { lat: lat, lng: long };
   let rad;
   let tempRad = parseInt(radius);
-  if(tempRad / 2 > 30){
+  if (tempRad / 2 > 30) {
     rad = 4;
-  } else if(tempRad / 2 > 10) {
+  } else if (tempRad / 2 > 10) {
     rad = 6;
   } else {
     rad = 7;
   }
+
   let map = new google.maps.Map($map, {
     zoom: rad,
-    center: myLatLng
+    center: myLatLng,
   });
 
   let coords;
@@ -110,13 +116,16 @@ function createMap(data, lat, long, radius) {
   }
 
   function addCircle(i) {
-    coords = {lat: data.features[i].geometry.coordinates[1], lng: data.features[i].geometry.coordinates[0]};
+    coords = { lat: data.features[i].geometry.coordinates[1],
+      lng: data.features[i].geometry.coordinates[0], };
     formattedDate = timeConverter(data.features[i].properties.time);
     status = data.features[i].properties.status;
     mag = data.features[i].properties.mag;
     let infowindow = new google.maps.InfoWindow({
-      content: `Date: ${formattedDate}<br>Status: ${status}<br>Magnitude: ${mag}`
+      content: `Date: ${formattedDate}<br>Status: ${status}<br>Magnitude: ${mag}`,
     });
+    let latOne = data.features[i].geometry.coordinates[1];
+    let latTwo = data.features[i].geometry.coordinates[0];
     let circle = new google.maps.Circle({
       strokeColor: '#009688',
       strokeOpacity: 0.8,
@@ -125,25 +134,22 @@ function createMap(data, lat, long, radius) {
       fillOpacity: 0.6,
       map: map,
       center: coords,
-      position: new google.maps.LatLng(data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]),
-      radius: 4 ** data.features[i].properties.mag
+      position: new google.maps.LatLng(latOne, latTwo),
+      radius: Math.pow(4, data.features[i].properties.mag),
     });
-    circle.addListener('click', function() {
+    circle.addListener('click', function () {
       infowindow.setPosition(coords);
       infowindow.open(map, circle);
     });
   }
-};
+}
 
 //Function to convert any js date into a well formatted one
 function timeConverter(time) {
   let dateToConvert = new Date(time);
-  let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   let year = dateToConvert.getFullYear();
   let month = months[dateToConvert.getMonth()];
   let date = dateToConvert.getDate();
-  let hour = dateToConvert.getHours();
-  let min = dateToConvert.getMinutes();
-  let sec = dateToConvert.getSeconds();
   return (month + ' ' + date + ', ' + year);
-};
+}
